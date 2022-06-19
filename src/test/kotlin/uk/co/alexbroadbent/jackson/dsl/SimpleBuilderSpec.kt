abf7820d7e9e13e6ce6ba90ed4b891eef82675b8
Alexander Broadbent <alexbroadbent14@gmail.com>
2022-06-19 17:08:22 +0100

    Add tests for deprecated methods

--- src/test/kotlin/uk/co/alexbroadbent/jackson/dsl/SimpleBuilderSpec.kt
+++ src/test/kotlin/uk/co/alexbroadbent/jackson/dsl/SimpleBuilderSpec.kt
@@ -60,4 +60,44 @@ class SimpleBuilderSpec : BaseSpec({
 
         json.toString() shouldBe """{"foo":"bar","ham":472,"spam":false}"""
     }
+
+    context("deprecated methods") {
+        should("build array") {
+            val json = array {
+                string("foo")
+                int(3)
+                long(124789124768219L)
+                double(3.14)
+                boolean(false)
+            }
+
+            val expected = mapper.createArrayNode()
+                .add("foo")
+                .add(3)
+                .add(124789124768219L)
+                .add(3.14)
+                .add(false)
+
+            json.node shouldBe expected
+        }
+
+        should("build object") {
+            val json = `object` {
+                string("string", "foobar")
+                int("int", 123)
+                long("long", 718492718496124L)
+                double("double", 3.14)
+                boolean("boolean", true)
+            }
+
+            val expected = mapper.createObjectNode()
+                .put("string", "foobar")
+                .put("int", 123)
+                .put("long", 718492718496124L)
+                .put("double", 3.14)
+                .put("boolean", true)
+
+            json.node shouldBe expected
+        }
+    }
 })
